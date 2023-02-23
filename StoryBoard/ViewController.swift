00f469ee02bb64ff5716bada9f66d6923d4b0e1c
benndip <obentabiayuk1@gmail.com>
2023-02-23 10:42:50 +0100

    Initial Commit

---
+++ StoryBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoryBoard
+//
+//  Created by MACBOOK on 23.02.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
